50375775ae20ce5937ffa3140538af1b1501e770
gracejoh <ganwynj@icloud.com>
2020-07-24 17:20:51 +0000

    secondary objective

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-### part one
-for i in range(10):
+### primary objective 
+for i in range(2):
   print ("outer loop | i : ", (i))
-  for j in range(10):
+  for j in range(2):
     print("outer loop | i : ", i, "inner loop | j : " , j)
     if i != 0 and j != 0 and (i%2) == 1 and (j%2) == 1 :
       print ("odd")
     
+### second
 
+char = input('>> enter a char: ')
+num = int(input('>>enter a positive number(>5): '))
+
+for i in range(1, num + 1):
+  text = ' '
+  for j in range(i):
+    text += char
+  print(text)
+
+for i in range(1, num + 1):
+  text = ' '
+  for j in range(num - i):
+    text += char
+  print(text)
+
+for i in range(1, num + 1):
+  text = ' '
+  for j in range(num + 1) :
+    text += char
+  print(text)
