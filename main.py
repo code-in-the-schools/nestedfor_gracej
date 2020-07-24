139f393a74b67029a1fd1b77b6f4a911ec2f1443
gracejoh <ganwynj@icloud.com>
2020-07-24 16:46:44 +0000

    part 2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 ### part one
-for i in range(6):
+for i in range(10):
   print ("outer loop | i : ", (i))
-  for j in range(6):
+  for j in range(10):
     print("outer loop | i : ", i, "inner loop | j : " , j)
-
+    if i != 0 and j != 0 and (i%2) == 0 and (j%2) == 0 :
+      print ("even")
+    
 
